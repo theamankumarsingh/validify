b93724039a2f5f12c56aeb4c3a87b123031b4f67
Pavithra Nair <pmpavithranair@gmail.com>
2023-02-08 01:28:53 +0530

    correction

--- clean.py
+++ clean.py
@@ -1,51 +1,13 @@
-import requests
-import re
+import pandas as pd
 
-headers = {
-    'user-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1',
-}
+def dict_to_workbook(working_data_dict):
+    df = pd.DataFrame(data=working_data_dict, index=[0])
+    df = (df.T)
+    df.to_excel('NGOs_bad_data.xlsx')
 
-def isValidURL(str):
- 
-    # Regex to check valid URL
-    regex = ("((http|https)://)(www.)?" +
-             "[a-zA-Z0-9@:%._\\+~#?&//=]" +
-             "{2,256}\\.[a-z]" +
-             "{2,6}\\b([-a-zA-Z0-9@:%" +
-             "._\\+~#?&//=]*)")
-     
-    # Compile the ReGex
-    p = re.compile(regex)
- 
-    # If the string is empty
-    # return false
-    if (str == None):
-        return False
- 
-    # Return if the string
-    # matched the ReGex
-    if(re.search(p, str)):
-        return True
-    else:
-        return False
- 
-
-def check(data_dict):
-    for key in data_dict:
-        val = data_dict.get(key)
-        print(val)
-        # Check if valid URL
-        if isValidURL(val):
-            print("VALID")
-            if requests.get(val, headers=headers).status_code == 200:
-                print("good")
-                response = requests.get(val, headers=headers)
-                print(response)
-                print(response.status_code)
-            else:
-                print("bad")
-        # If not valid URL
-        else:
-            print("Not valid URL")
-            print(val)
+def workbook_to_dict(working_data_dict):
+    xls = ExcelFile('NGOs_bad_data.xlsx')
+    df = xls.parse(xls.sheet_names[0])
+    working_data_dict = df.to_dict()
+    return working_data_dict
 
