68af3f644dca910145bc50ed1d9eb6b338e58fb0
Aman Kumar Singh <amankumarsingh.professional@gmail.com>
2023-02-07 22:37:53 +0530

    add: website check with exception handling

--- finalization.py
+++ finalization.py
@@ -1,3 +1,10 @@
+import requests
+from progressbar import progressbar
+
+header = {
+    'user-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1',
+}
+
 def modify(dataset, data_dict):
     #modify dataset
     for row in range(len(dataset)):
@@ -5,3 +12,25 @@ def modify(dataset, data_dict):
         dataset[row][5]=data_dict[dataset[row][0]]
         dataset[row]=tuple(dataset[row])
     return dataset
+
+def check(dataset):
+    #check if websites are valid
+    test_res=[]
+    print("Checking websites...")
+    for i in progressbar(range(len(dataset))):
+        element=dataset[i]
+        if element[5]==None or element[5]=="Not Available":
+            test_res.append((element[5],"Skip",""))
+            continue;
+        else:
+            try:
+                response = requests.get(element[5],headers=header)
+                stat_code=response.status_code
+            except Exception as exception:
+                test_res.append((element[5],"exception",type(exception).__name__))
+                continue;
+            if stat_code == 200:
+                test_res.append((element[5],"good",str(stat_code)))
+            else:
+                test_res.append((element[5],"bad",str(stat_code)))
+    return test_res
--- script.py
+++ script.py
@@ -14,9 +14,11 @@ dataset,data_dict=preparation.prepare(sheet)
 
 #post-processing
 dataset=finalization.modify(dataset,data_dict)
+test_res=finalization.check(dataset)
 
 #write sheet to workbook
 wb_new = openpyxl.Workbook()
+wb_new.create_sheet('Test')
 sheet_new = wb_new['Sheet']
 for row in range(len(dataset)):
     sheet_new['A'+str(row+1)]=dataset[row][1]
@@ -24,4 +26,9 @@ for row in range(len(dataset)):
     sheet_new['C'+str(row+1)]=dataset[row][3]
     sheet_new['D'+str(row+1)]=dataset[row][4]
     sheet_new['E'+str(row+1)]=dataset[row][5]
+sheet_new = wb_new['Test']
+for row in range(len(test_res)):
+    sheet_new['A'+str(row+1)]=test_res[row][0]
+    sheet_new['B'+str(row+1)]=test_res[row][1]
+    sheet_new['C'+str(row+1)]=test_res[row][2]
 wb_new.save('NGOs_Name_with_Address_cleaned.xlsx')
