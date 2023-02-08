d7441a26c996884681240468662c564e66aa0940
Aman Kumar Singh <amankumarsingh.professional@gmail.com>
2023-02-08 21:25:26 +0530

    update .gitignore and requirements.txt, fix worksheet name in clean.py

--- clean.py
+++ clean.py
@@ -4,7 +4,7 @@ import openpyxl
 def dict_to_workbook(working_data_dict):
     df = pd.DataFrame(data=working_data_dict, index=[0])
     df = (df.T)
-    df.to_excel('NGOs_bad_data.xlsx')
+    df.to_excel('NGOs_Name_with_Address_bad.xlsx')
 
 def workbook_to_dict(sheet_name):
     data_dict={}
